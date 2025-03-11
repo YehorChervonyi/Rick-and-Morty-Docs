26c92604e67c5aa8c1322fc78339074f1af83bcb
Yehor Chervony <chervony.egor@gmail.com>
2025-03-12 01:11:08 +0200

    Added API endpoints

---
+++ lib/core/network/api_endpoints.dart
@@ -0,0 +1,7 @@
+class ApiEndpoints{
+  static const String _baseUrl = "https://rickandmortyapi.com/api/";
+
+  static const String character = "${_baseUrl}character";
+  static const String location = "${_baseUrl}location";
+  static const String episode = "${_baseUrl}episode";
+}
