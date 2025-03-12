f9bd6566afd3577c0ac662e25f6b2b44c97b2dcc
Yehor Chervony <chervony.egor@gmail.com>
2025-03-13 00:47:52 +0200

    Changed fetch chars controller ,added fetch by page and name with page

--- lib/controllers/character_controller.dart
+++ lib/controllers/character_controller.dart
@@ -1,13 +1,21 @@
-import 'package:flutter/material.dart';
 import 'package:rick_and_morty_docs/models/character_model.dart';
 import 'package:rick_and_morty_docs/services/character_service.dart';
 
 class CharacterController {
   CharacterModel? characters;
 
-  Future<CharacterModel> fetchCharacters() async {
+  Future <CharacterModel>  fetchCharacters(int page) async {
     try {
-      characters = await CharacterService().fetchCharacters();
+      characters = await CharacterService().fetchCharacters(page);
+      return characters!;
+    } catch (e) {
+      throw Exception("Error fetching characters: $e");
+    }
+  }
+
+  Future<CharacterModel> fetchCharactersByName(String name, int page) async {
+    try {
+      characters = await CharacterService().fetchCharactersByName(name, page);
       return characters!;
     } catch (e) {
       throw Exception("Error fetching characters: $e");
