db5320abb85d3c302cbd2ba286c2c0cb02653904
Yehor Chervony <chervony.egor@gmail.com>
2025-03-13 00:46:45 +0200

    Changed fetch chars,added fetch by page

--- lib/services/character_service.dart
+++ lib/services/character_service.dart
@@ -4,16 +4,14 @@ import 'package:rick_and_morty_docs/core/network/api_endpoints.dart';
 import 'package:rick_and_morty_docs/models/character_model.dart';
 
 class CharacterService{
-  Future<CharacterModel> fetchCharacters() async{
-    // var response = await http.get(Uri.parse(ApiService.baseUrl + ApiEndpoints.character));
-    var response = await http.get(Uri.parse(ApiEndpoints.character));
-
+  Future<CharacterModel> fetchCharacters(int page) async{
+    var response = await http.get(Uri.parse(ApiEndpoints.character+"/?page=${page}"));
     var jsonData = json.decode(response.body);
-    // if (response.)
-    // print(response.body);
-    // print(jsonData);
-    // var test = jsonData.map((json) => CharacterModel.fromJson(json)).toList();
-    // print(test);
+    return CharacterModel.fromJson(jsonData);
+  }
+  Future<CharacterModel> fetchCharactersByName(String name, int page) async{
+    var response = await http.get(Uri.parse(ApiEndpoints.character+"/?name=${name}&page=${page}"));
+    var jsonData = json.decode(response.body);
     return CharacterModel.fromJson(jsonData);
   }
 }
