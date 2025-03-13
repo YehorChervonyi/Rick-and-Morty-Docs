0ae814a761d6feea16864b9f1b2c9e9b9934d05b
Yehor Chervony <chervony.egor@gmail.com>
2025-03-13 16:42:12 +0200

    Using CharacterView as main page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:rick_and_morty_docs/views/characters/characters_view.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(),
+      home: CharacterView(),
     );
   }
 }
